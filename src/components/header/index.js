57b41be7fd4eef359616018eae7ba01e487b530f
gandarfh <jgdsantosferreira@gmail.com>
2020-05-15 18:05:57 -0300

    add: Criação de estrutura de header e footer e inserção nas rotas

---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Header() {
+  return (
+    <>Header</>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+
+import Header from './components/header';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Aluno from './pages/Aluno';
@@ -12,15 +15,17 @@ import ProcessoSeletivo from './pages/ProcessoSeletivo';
 export default function Routes() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/aluno" component={Aluno}/>
-        <Route path="/contato" component={Contato}/>
-        <Route path="/faca-parte" component={FacaParte}/>
-        <Route path="/noticias" component={Noticias}/>
-        <Route path="/processo-seletivo" component={ProcessoSeletivo}/>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/aluno" component={Aluno} />
+        <Route path="/contato" component={Contato} />
+        <Route path="/faca-parte" component={FacaParte} />
+        <Route path="/noticias" component={Noticias} />
+        <Route path="/processo-seletivo" component={ProcessoSeletivo} />
       </Switch>
+      <Footer />
     </BrowserRouter>
   )
 }
