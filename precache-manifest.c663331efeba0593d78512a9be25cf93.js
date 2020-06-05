bfe0eaa827356e16195722977b81ef5d96553527
viniciussntn <vsviniciuslima@usp.br>
2020-06-05 18:08:44 -0300

    Updates

---
+++ precache-manifest.c663331efeba0593d78512a9be25cf93.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f570628ad79d8707262162eddf776d95",
+    "url": "/site-cpe/index.html"
+  },
+  {
+    "revision": "6e64d2a317c6affff36d",
+    "url": "/site-cpe/static/css/2.9e5c3921.chunk.css"
+  },
+  {
+    "revision": "a502d57b8b30fb4dbec7",
+    "url": "/site-cpe/static/css/main.80644a4c.chunk.css"
+  },
+  {
+    "revision": "6e64d2a317c6affff36d",
+    "url": "/site-cpe/static/js/2.1ceae7af.chunk.js"
+  },
+  {
+    "revision": "acc33c18036bd6a21f826065f5e19315",
+    "url": "/site-cpe/static/js/2.1ceae7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a502d57b8b30fb4dbec7",
+    "url": "/site-cpe/static/js/main.1b4bcd56.chunk.js"
+  },
+  {
+    "revision": "74ac6b5795215c725754",
+    "url": "/site-cpe/static/js/runtime-main.77763265.js"
+  },
+  {
+    "revision": "192f7810a7745571fb848b20b821a761",
+    "url": "/site-cpe/static/media/Triangulos@2xTransparente.192f7810.png"
+  },
+  {
+    "revision": "35b39cc77487d17cf70e55ed4f962d30",
+    "url": "/site-cpe/static/media/background-img-2.35b39cc7.jpg"
+  },
+  {
+    "revision": "50e716f6db18c7120d187397a177835e",
+    "url": "/site-cpe/static/media/cmkt.50e716f6.png"
+  },
+  {
+    "revision": "753253459fc1b248c1bff1494d088774",
+    "url": "/site-cpe/static/media/cool-girl.75325345.gif"
+  },
+  {
+    "revision": "87cf60dfd52f895220736c7c68fbd74b",
+    "url": "/site-cpe/static/media/coruja.87cf60df.png"
+  },
+  {
+    "revision": "5902c55582af16e551d253aec2a15fcc",
+    "url": "/site-cpe/static/media/gp.5902c555.png"
+  },
+  {
+    "revision": "6b410ef3c3383b267e9cf9ac34d43509",
+    "url": "/site-cpe/static/media/inovatec.6b410ef3.png"
+  },
+  {
+    "revision": "dd90fa4624a1f2cb9d3a9a5070ff9e78",
+    "url": "/site-cpe/static/media/jurfin.dd90fa46.png"
+  },
+  {
+    "revision": "33859b5e0f606646ad8463232b0c4bff",
+    "url": "/site-cpe/static/media/logo-cursinho.33859b5e.png"
+  },
+  {
+    "revision": "b41037436f092f27d7a1becdd82dcb03",
+    "url": "/site-cpe/static/media/ped.b4103743.png"
+  },
+  {
+    "revision": "4f141abf11f77aab56b11ed6c54c12ab",
+    "url": "/site-cpe/static/media/post-1.4f141abf.jpg"
+  },
+  {
+    "revision": "aa3d9694c50e9cc7ae370d8ebb22a0cf",
+    "url": "/site-cpe/static/media/post-1.aa3d9694.png"
+  },
+  {
+    "revision": "8981c08277ece964597c7bbeebb3c03d",
+    "url": "/site-cpe/static/media/post-2.8981c082.jpg"
+  },
+  {
+    "revision": "9542a46bf994b4a8590972e3f1d586cc",
+    "url": "/site-cpe/static/media/post-2.9542a46b.png"
+  },
+  {
+    "revision": "13317a7b1ee14f5f054aef66ba8009d1",
+    "url": "/site-cpe/static/media/post-3.13317a7b.png"
+  },
+  {
+    "revision": "b5eabc3e81c954ff7305c3484b81e150",
+    "url": "/site-cpe/static/media/post-4.b5eabc3e.png"
+  },
+  {
+    "revision": "692aab7461a16275eab6c8ccd4742fa8",
+    "url": "/site-cpe/static/media/post-5.692aab74.png"
+  },
+  {
+    "revision": "9a16549bc199491e3385588cc5e8f3b2",
+    "url": "/site-cpe/static/media/proj.9a16549b.png"
+  }
+]);
